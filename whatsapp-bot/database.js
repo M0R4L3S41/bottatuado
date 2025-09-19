ac1762b4992eee0a36df61a49c5512b4b426bd03
M0R4L3S41 <58787753+M0R4L3S41@users.noreply.github.com>
2025-09-18 19:39:04 -0600

    Update database.js

--- whatsapp-bot/database.js
+++ whatsapp-bot/database.js
@@ -10,11 +10,11 @@ class DatabaseManager {
     async init() {
         try {
             this.pool = mysql.createPool({
-                host: process.env.DB_HOST,
+                host: process.env.MYSQL_HOST,
                 port: process.env.DB_PORT,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_NAME,
+                user: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_DATABASE,
                 waitForConnections: true,
                 connectionLimit: 10,
                 queueLimit: 0
@@ -965,5 +965,6 @@ module.exports = {
     actualizarConfiguracionEspecial,
     debeUsarEnmarcadoAutomatico,
     debeSubirApiAutomatico
+
 
 };
